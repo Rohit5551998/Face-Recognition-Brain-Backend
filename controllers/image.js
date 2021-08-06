85bf4c782a63aec559177cb73463ed82a3f5ed65
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-08-06 17:43:56 +0530

    Security Changes

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'd04792060c624b3b83127a34c01b5523'
+    apiKey: 'process.env.API_CLARIFAI'
 });
 
 const handleApiCall = (req, res) => {
