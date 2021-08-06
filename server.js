0d05a32e4c06004ac5806f03864926c4ede94780
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-08-06 17:16:12 +0530

    Minor Fix

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const db = knex({
     // Remote Connection
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
